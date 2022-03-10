a5ddcd335c59910eb0384c36201dc2a43ff70000
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-03-10 12:27:38 -0500

    Update configuracion.js

--- configuracion.js
+++ configuracion.js
@@ -1,5 +1,5 @@
 const config = {
-    DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority',
+    DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority&replicaSet=rs-ds012345',
     PUERTO: 3000
 }
 
